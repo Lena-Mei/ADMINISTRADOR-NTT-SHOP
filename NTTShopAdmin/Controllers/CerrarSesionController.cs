fb296d79ee70997b3151c3d02e2e277fd670ea6b
Miguel Angel Martin Gimenez <mmargime@emeal.nttdata.com>
2024-05-29 10:31:09 +0200

    Agregado la funcionalidad de cerrar sesion en el nav bar y su controlador

---
+++ NTTShopAdmin/Controllers/CerrarSesionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using NTTShopAdmin.Models;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Web.UI.WebControls;
+
+
+namespace NTTShopAdmin.Controllers
+{
+    public class CerrarSesionController : Controller
+    {  
+        public ActionResult Cerrarsesion()
+        {
+            Session["IdUser"] = null;
+            Session["LoginUser"] = null;
+            return RedirectToAction("Login", "Login");
+        }
+ 
+    }
+}
+
+
+    
